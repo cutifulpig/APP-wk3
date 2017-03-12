29f206cae7f8443db226008eb70df21f82ab86a2
cutifulpig <david0910932933@gmail.com>
2017-03-12 18:48:24 +0800

    1st

---
+++ 3.js
@@ -0,0 +1,13 @@
+//object
+
+let name,phone;
+let info={
+  name,
+  phone,printinfo(){
+    console.log(`Name:${this.name},Phone:${this.phone}`);
+  }
+}
+
+info.name="ntue";
+info.phone="2732-1104";
+info.printinfo();
